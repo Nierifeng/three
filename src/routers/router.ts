1211aa5593c0a8861ed6018f0be4f17f3f84bf14
NieRiFeng <17673376360@163.com>
2023-07-07 13:49:18 +0800

    modify deprecated properties

--- src/routers/router.ts
+++ src/routers/router.ts
@@ -1,28 +1,36 @@
-import { createRouter, createWebHashHistory, createWebHistory, RouteRecordRaw } from "vue-router";
+import {
+  createRouter,
+  createWebHashHistory,
+  createWebHistory,
+  RouteRecordRaw,
+} from "vue-router";
 
 // * 导入所有router
-const metaRouters = import.meta.globEager("./modules/*.ts");
+// const metaRouters = import.meta.globEager("./modules/*.ts");
+const metaRouters: Record<string, any> = import.meta.glob("./modules/*.ts", {
+  eager: true,
+});
 
 // * 处理路由
 export const routerArray: RouteRecordRaw[] = [];
-Object.keys(metaRouters).forEach(item => {
-  Object.keys(metaRouters[item]).forEach((key: any) => {
-    routerArray.push(...metaRouters[item][key]);
-  });
+Object.keys(metaRouters).forEach((key) => {
+  routerArray.push(...metaRouters[key].default);
 });
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    redirect: { name: 'basis' }
-  }, ...routerArray];
+    path: "/",
+    redirect: { name: "basis" },
+  },
+  ...routerArray,
+];
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
   strict: false,
   // 切换页面，滚动到最顶部
-  scrollBehavior: () => ({ left: 0, top: 0 })
+  scrollBehavior: () => ({ left: 0, top: 0 }),
 });
 
 export default router;
